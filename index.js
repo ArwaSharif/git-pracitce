24c904b9104a5e1e3a05094b79c7a6663fa0449e
Arwa <arwamsharif@gmail.com>
2023-03-07 10:51:01 -0600

    just practicing

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('practicing my git commands')
+
+// git init to initiate a repo
+
+// ls -a Or git status to see the status of rep
+
+// to undo or make a folder not a git-repo: 
+    // "rm -rf .git"
+
+
+// adding/staging a file to a repo= to commit a file by using:
+    // "git add" = 
+        // "git add ."" = adding current folder and everything inside of it. will stage all the folders in the master if there are more than one 
+        // "git add -A" = will commit everything even is you are in a sub folder of th repo-
